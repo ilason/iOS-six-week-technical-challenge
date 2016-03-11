c5f3ad67742ee34faefd403761e37de599f1ebb7
Aaron Eliason <aaroneliason@hotmail.com>
2016-03-11 10:03:35 -0700

    Finished NameEntry.swift
    Starting on a EntryController for names to randomize

---
+++ Six Week Technical Challenge/EntryController.swift
@@ -0,0 +1,15 @@
+//
+//  EntryController.swift
+//  Six Week Technical Challenge
+//
+//  Created by Aaron Eliason on 3/11/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import Foundation
+
+class EntryController {
+    var entries: [NameEntries]
+    
+    
+}
---
+++ Six Week Technical Challenge/NameEntries.swift
@@ -0,0 +1,40 @@
+//
+//  NameEntries.swift
+//  Six Week Technical Challenge
+//
+//  Created by Aaron Eliason on 3/11/16.
+//  Copyright © 2016 IlasonApps. All rights reserved.
+//
+
+import Foundation
+
+class NameEntries: Equatable {
+    private let nameOfPersonKey = "nameOfPerson"
+    
+    var nameOfPerson: String
+    
+    init(nameOfPerson: String) {
+        self.nameOfPerson = nameOfPerson
+    }
+    
+    init?(dictionary: Dictionary<String, AnyObject>) {
+        guard let nameOfPerson = dictionary[nameOfPersonKey] as? String else {
+            self.nameOfPerson = ""
+            return nil
+    }
+        self.nameOfPerson = nameOfPerson
+}
+
+    func dictionaryCopy() -> Dictionary<String, AnyObject> {
+        let dictionary = [
+            nameOfPerson : self.nameOfPerson
+        
+        ]
+        
+        return dictionary
+    }
+}
+
+func ==(lhs: NameEntries, rhs: NameEntries) -> Bool {
+    return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+}
